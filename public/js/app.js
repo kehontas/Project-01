f9564c8d84361a5a2a7a6cffd9ad722fe8e9782e
kehontas <kehontas@gmail.com>
2015-12-16 11:34:42 -0800

    working on create

--- public/js/app.js
+++ public/js/app.js
@@ -3,22 +3,36 @@ $(document).ready(function() {
    $.get('/api/beans').success(function (beans){
   	beans.forEach(function(bean) {
   		renderBean(bean);
-  	});
-});
-   
-//   function renderBean(bean) {
-//   console.log('rendering bean:', bean);
-// }
+	  	});
+	});
+
+ 	function renderBean(bean) {
+	console.log('rendering bean:', bean);
+}
+
 $.ajax({
   	method: "GET",
 	url: "api/beans",
   	success: function (data) { 	
   	console.log("this is my data:", data);	
   	data.forEach(function(e){
-		$("#beans").append("<h4>" + "<a href='/beans/"+ e._id + "'>" + e.beanName + "</a>" + "<hr>" );
+		$(".beans").append("<h4>" + "<a href='/beans/"+ e._id + "'>" + e.beanName + "</a>" + "<hr>" );
   			
   			});	
 		}
-		});
- 	});
+	});
+
+var beanID = $(this).closest('.bean').attr('data.data');
+
+$.ajax({
+	method:"POST",
+	url: "api/beans/" + beanID + "/cafe" ,
+	success: function(data){
+		data.forEach(function(e){
+			$("#cafeList").append( e.cafeName + e.cafeCity + e.cafeUrl );
+			});
+		}
+	});
+
+});
 
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', function homepage (req, res) {
 });
 
 
-app.get('/api/beans', function albumsIndex(req, res) {
+app.get('/api/beans', function beansIndex(req, res) {
   db.Bean.find({}, function(err, beans) {
     res.json(beans);
   	});
@@ -39,6 +39,24 @@ app.get('/beans/:id', function (req,res) {
 			//console.log(bean.beanLocations.length );
   		res.render('beans/show', bean);
 	});  		
+});
+
+app.post('/beans/:id', function (req,res) {
+	console.log('req this',req.body);
+	
+	db.Bean.findOne({_id: req.params.id}, function createCafe(err, cafe){
+		if(err){console.log('error in cafe', err);}
+
+		var cafe = new db.Cafe(req.body);
+		beans.cafe.push(cafe);
+		beans.save (function (err, savedCafe) {
+		  
+			if (err) {console.log('error in saving cafe: ', err); }
+         	console.log(savedEvent, 'Bean has a new serving location! : ');
+		
+            res.json(cafe);
+		});
+	});
 });
 
 app.listen(process.env.PORT || 3000, function () {
