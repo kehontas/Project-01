e0c0090f07d6dc42d12070f9604e137d401d29da
kehontas <kehontas@gmail.com>
2015-12-17 11:50:07 -0800

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/cafeJSON");
+mongoose.connect( process.env.MONGOLAB_URI ||
+                      process.env.MONGOHQ_URL || 
+                      "mongodb://localhost/cafeJSON");
+
 var Bean = require('./bean');
 var Cafe = require('./cafe');
 
